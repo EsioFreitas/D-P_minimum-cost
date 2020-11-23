11cc61e382c96916d1c9cac7ae179a283a9a6e4b
EsioFreitas <esio.gustavo@gmail.com>
2020-11-23 18:39:47 -0300

    Fix algorithms generates

--- src/algorithms/findPath.js
+++ src/algorithms/findPath.js
@@ -1,25 +1,21 @@
 export default function findPath(matrix) {
-    console.log(matrix)
-    const lines = matrix.length;
-    const columns = matrix[0].length;
+  console.log(matrix);
+  const lines = matrix.length;
+  const columns = matrix[0].length;
 
-    let i = lines - 1;
-    let j = columns - 1;
+  let i = lines - 1;
+  let j = columns - 1;
 
-    const path = [];
-    
-    while(i + j > 0) {
-        path.push([i, j]);
+  const path = [];
 
-        if (j === 0)
-            i--;
-        else if (i === 0)
-            j--;
-        else if (matrix[i-1][j] <= matrix[i][j-1])
-            i--;
-        else
-            j--;
-    }
-    path.push([0, 0])
-    return path;
+  while (i + j > 0) {
+    path.push([i, j]);
+
+    if (j === 0) i--;
+    else if (i === 0) j--;
+    else if (matrix[i - 1][j] <= matrix[i][j - 1]) i--;
+    else j--;
+  }
+  path.push([0, 0]);
+  return path;
 }
--- src/algorithms/generateCosts.js
+++ src/algorithms/generateCosts.js
@@ -1,11 +1,13 @@
 export default function generateCosts() {
-    const min = 4;
-    const max = 8;
-    const numLines = Math.floor(Math.random() * (max - min + 1)) + min;
-    const costs = [];
-    for(let i = 0; i < numLines; i++) {
-        const line = Array.from({ length: numLines }, () =>  Math.floor(Math.random() * 99));
-        costs.push(line);
-    }
-    return costs;
+  const min = 4;
+  const max = 8;
+  const numLines = Math.floor(Math.random() * (max - min + 1)) + min;
+  const costs = [];
+  for (let i = 0; i < numLines; i++) {
+    const line = Array.from({ length: numLines }, () =>
+      Math.floor(Math.random() * 99)
+    );
+    costs.push(line);
+  }
+  return costs;
 }
