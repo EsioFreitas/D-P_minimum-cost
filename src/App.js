c97e943c90d44c02137a216da7574040cc071b60
EsioFreitas <esio.gustavo@gmail.com>
2020-11-23 18:40:27 -0300

    Fix general bugs

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import generateCosts from './algorithms/generateCosts';
-import findMinCost from './algorithms/findMinCost';
-import findPath from './algorithms/findPath';
+import generateCosts from "./algorithms/generateCosts";
+import findMinCost from "./algorithms/findMinCost";
+import findPath from "./algorithms/findPath";
 
 function App() {
   const [matrix, setMatrix] = useState([]);
   const [path, setPath] = useState([]);
-  const [result, setResult] = useState('--')
+  const [result, setResult] = useState("--");
 
   useEffect(() => {
     newMap();
     // eslint-disable-next-line
   }, []);
 
-
   function handleReset() {
-    setPath([])
-    setResult('--')
+    setPath([]);
+    setResult("--");
   }
 
   function newMap() {
-    handleReset()
+    handleReset();
     const costs = generateCosts();
     setMatrix(costs);
   }
 
   function handlePlay() {
-    const result = findMinCost(matrix, matrix.length, matrix[0].length)
+    const result = findMinCost(matrix, matrix.length, matrix[0].length);
     const path = findPath(result[1]);
     setPath(path);
     setResult(result[0]);
   }
 
   function isInPath(i, j) {
-    const index = path.findIndex(coord => coord[0] === i && coord[1] === j);
+    const index = path.findIndex((coord) => coord[0] === i && coord[1] === j);
     return index >= 0;
   }
 
   function getColor(i, j) {
-    if (i + j === 0)
-      return '#03787C';
-    else if (isInPath(i, j))
-      return '#03787C';
-    else if ((i + 1)*(j + 1) === matrix.length*matrix.length )
-      return '#e68057';
-    return '#fff';
+    if (i + j === 0) return "#03787C";
+    else if (isInPath(i, j)) return "#03787C";
+    else if ((i + 1) * (j + 1) === matrix.length * matrix.length)
+      return "#e68057";
+    return "#fff";
   }
 
   return (
@@ -66,28 +63,35 @@ function App() {
             primeira.
           </p>
         </div>
-        <h4 className="text-center bold" ><b>O menor caminho é {result}</b></h4>
+        <h4 className="text-center bold">
+          <b>O menor caminho é {result}</b>
+        </h4>
         <div className="row">
           <div className="col d-flex justify-content-center mt-3">
             <div>
               {matrix.map((line, i) => (
-                  <div key={i+1} className="d-flex">
-                    {
-                      line.map((cost, j) => (
-                        <div key={j+1} className="tablet d-flex justify-content-center align-items-center" style={{ backgroundColor: getColor(i, j)}}>
-                          {cost}
-                        </div>
-                      ))
-                    }
-                  </div>
-                )
-              )}
+                <div key={i + 1} className="d-flex">
+                  {line.map((cost, j) => (
+                    <div
+                      key={j + 1}
+                      className="tablet d-flex justify-content-center align-items-center"
+                      style={{ backgroundColor: getColor(i, j) }}
+                    >
+                      {cost}
+                    </div>
+                  ))}
+                </div>
+              ))}
             </div>
           </div>
         </div>
         <div className="d-flex justify-content-center mt-5 align-items-center">
-          <button className="btn handle-btn finish mr-5" onClick={newMap}>Novo Mapa</button>
-          <button className="btn handle-btn start" onClick={handlePlay}>Jogar</button>
+          <button className="btn handle-btn finish mr-5" onClick={newMap}>
+            Novo Mapa
+          </button>
+          <button className="btn handle-btn start" onClick={handlePlay}>
+            Jogar
+          </button>
         </div>
       </div>
     </div>
